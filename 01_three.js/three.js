edabb1383dfa8c4743a5de5ce83edbf97dc0e12f
shita-aki <hiro.dev.release14@gmail.com>
2016-01-05 23:11:45 +0900

    ■WebGL01 構成をもっと変更。 引き続き、以下も参考にメッシュを2つ表示させてみた。 http://www.ibm.com/developerworks/jp/web/library/wa-webgl2/index.html

--- 01_three.js/three.js
+++ 01_three.js/three.js
@@ -1,25 +1,90 @@
 /*globals THREE */
 /*eslint-env browser */
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var scene;
+var camera;
+var directionalLight;
+var renderer;
+var cube0;
+var cube1;
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+function display () {
+	initSpace();
 
-camera.position.z = 5;
+	// キューブ0を作成しシーンに追加
+	cube0 = initCube(2, 1, 1, 0x0000ff);
+	scene.add(cube0);
+
+	// キューブ0を作成しシーンに追加
+	cube1 = initCube(1, 1, 1, 0xff0000);
+	scene.add(cube1);
+
+	// 各キューブの位置を設定
+	cube0.position.x = -2;
+	cube0.position.z = -1;
+	cube1.position.x = 1;
+	cube1.position.z = 0;
+
+	// requestAnimationFrame使わないで、自前で一定間隔で表示させる
+	setInterval(function(){
+		render();
+	}, 33);
+}
 
 function render () {
-	requestAnimationFrame(render);
+/*
+ * 表示する
+ */
+
+//	requestAnimationFrame(render);
+
+	// 表示する
 	renderer.render(scene, camera);
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+
+	// 各キューブの回転角を設定する
+	cube0.rotation.x += 0.01;
+	cube0.rotation.y += 0.01;
+	cube1.rotation.x += 0.01;
+	cube1.rotation.y += 0.02;
+	cube1.rotation.z += 0.03;
 }
-// render();
-			
+
+function initSpace () {
+/*
+ * Scene、Camera、Light、Rendererの初期設定をまとめてやる
+ */
+	// Scene作る
+	scene = new THREE.Scene();
+
+	// Camera作って設定する
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);	
+	camera.position.z = 5;
+
+	// Light作って設定して、sceneに追加する
+	directionalLight = new THREE.DirectionalLight( 0xffffff );
+	directionalLight.position.set( 0, 50, 50 );
+	scene.add( directionalLight );
+
+	// Renderer作って設定して、DOMに追加する
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+}
+
+function initCube (tx, ty, tz, tcolor) {
+/*
+ * 直方体のmeshを作成する
+ *   x, y, z: サイズ
+ *   tcolor: 色
+ */
+	// geoetry作成する
+	var geometry = new THREE.BoxGeometry(tx, ty, tz);
+
+	// material作成する
+	var material = new THREE.MeshLambertMaterial( { color: tcolor } );
+	//var material = new THREE.MeshBasicMaterial({color: 0xff0000});
+
+	// mesh作成する
+	return new THREE.Mesh(geometry, material);	
+}
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ https://developer.mozilla.org/ja/docs/Web/API/WebGL_API
 
 ##次はthree.js
 http://threejs.org/docs/index.html#Manual/Introduction/Creating_a_scene
-
+http://www.ibm.com/developerworks/jp/web/library/wa-webgl2/index.html
