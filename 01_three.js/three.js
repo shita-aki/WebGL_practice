47523032da0fc46c11d7ac1db43709f99e271d7b
shita-aki <hiro.dev.release14@gmail.com>
2016-01-07 22:39:42 +0900

    ■WebGL01 影の表示を直した。 ライトの設定で、potisionを正規化して座標を小さくしたせいで、ほとんどのmeshが光点(？)より上に行ってしまったせいだと思われる。 最初してなかったんだけど、IBMのページで正規化されてたのでつい…。
    その他、ライト位置と強さ(色)、meshのサイズや位置を修正。

--- 01_three.js/three.js
+++ 01_three.js/three.js
@@ -10,19 +10,20 @@ var renderer;			// レンダラ
 var multi, cube0, cube1, cube2, floor;	// メッシュとかオブジェクト
 
 function display () {
+	// 場を作成！
 	initSpace();
 
 	// キューブ0を作成
-	cube0 = initCube(2, 1, 1, 0x0000ff);
+	cube0 = initCube(3, 1, 1, 0x3333ff);
 	// キューブ1を作成
 	cube1 = initCube(1, 1, 1, 0xff0000);
 	// キューブ2を作成
-	cube2 = initCubeC(2, 2, 3, [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff]);
+	cube2 = initCubeC(0.5, 5, 4, [0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff00ff, 0x00ffff]);
 
 	// 各キューブの位置を設定
-	cube0.position.set(-2, -1, -1);
-	cube1.position.set(1, 1, 0);
-	cube2.position.set(0, 2, -2);
+	cube0.position.set(-3, -1, -1);
+	cube1.position.set(1, -2, 0);
+	cube2.position.set(0, 2.5, 0);
 
 	// 各キューブをまとめたシーングラフを作成し、シーンに追加
 	multi = new THREE.Object3D();
@@ -35,7 +36,7 @@ function display () {
 	// フロアを作成しシーンに追加
 	floor = initFloor(20, 25, 0xcfcfcf);
     floor.rotation.x = Math.PI/2;
-    floor.position.y = -2;
+    floor.position.y = -4;
 	scene.add(floor);
 
 	// requestAnimationFrame使わないで、自前で一定間隔で表示させる
@@ -77,25 +78,26 @@ function initSpace () {
 
 	// Light作って設定して、sceneに追加する
 	directionalLight = new THREE.DirectionalLight( 0xffffff );
-	directionalLight.position.set( 0, 20, 0).normalize();
-
+	directionalLight.position.set(-10, 20, 5);
+	// 影を付ける設定を行う
     directionalLight.castShadow = true;
-    directionalLight.shadowDarkness = 0.5;
-    directionalLight.shadowCameraRight = 20;
-    directionalLight.shadowCameraLeft = -20;
-    directionalLight.shadowCameraTop = 20;
-    directionalLight.shadowCameraBottom = -20;
-    directionalLight.shadowCameraNear = 0;
-    directionalLight.shadowCameraFar = 30;
+    directionalLight.shadowDarkness = 1.0;
+    directionalLight.shadowCameraRight = 30;
+    directionalLight.shadowCameraLeft = -30;
+    directionalLight.shadowCameraTop = 30;
+    directionalLight.shadowCameraBottom = -30;
+    directionalLight.shadowCameraNear = 5;
+    directionalLight.shadowCameraFar = 100;
 
+	// 環境光作って設定して、sceneに追加する
 	scene.add(directionalLight);
-	ambientLight = new THREE.AmbientLight(0x101010);
+	ambientLight = new THREE.AmbientLight(0x555555);
 	scene.add(ambientLight);
 
 	// Renderer作って設定して、DOMに追加する
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.enabled = true;		// 影をレンダリングする
 	document.body.appendChild(renderer.domElement);
 }
 
@@ -107,14 +109,16 @@ function initCube (tx, ty, tz, tcolor) {
  */
 	// geoetry作成する
 	var geometry = new THREE.BoxGeometry(tx, ty, tz);
-
 	// material作成する
 	var material = new THREE.MeshLambertMaterial( { color: tcolor } );
-	//var material = new THREE.MeshBasicMaterial({color: 0xff0000});
-
 	// mesh作成する
 	var tmesh = new THREE.Mesh(geometry, material);	
-	tmesh.castShadow = true;
+
+	// 影の設定を行う
+	tmesh.castShadow = true;		// 影を投影する
+	tmesh.receiveShadow = true;	// 影が投影される
+
+	// 作成したmeshをリターンする
 	return tmesh;
 }
 
@@ -126,29 +130,39 @@ function initCubeC (tx, ty, tz, tcolors) {
  */
 	// geoetry作成する
 	var geometry = new THREE.BoxGeometry(tx, ty, tz);
-
 	// material作成する
     tcolors.forEach( function(color, idx)
     	{ geometry.faces[2 * idx + 1].color.setHex(color);
     	  geometry.faces[2 * idx].color.setHex(color);}
     );
 	var material = new THREE.MeshLambertMaterial({vertexColors: THREE.FaceColors});
-
 	// mesh作成する
 	var tmesh = new THREE.Mesh(geometry, material);
-	tmesh.castShadow = true;
+
+	// 影の設定を行う
+	tmesh.castShadow = true;		// 影を投影する
+	tmesh.receiveShadow = true;	// 影が投影される
+
+	// 作成したmeshをリターンする
 	return tmesh;
 }
 
 function initFloor (tx, ty, tcolor) {
 /*
- * 
+ * 平面のmeshを作成する
  */
+	// geoetry作成する
 	var geometry = new THREE.PlaneGeometry(tx, ty);
+	// material作成する
 	var material = new THREE.MeshLambertMaterial({color: tcolor});
+	// mesh作成する
 	var tfloor = new THREE.Mesh(geometry, material);
 	
+	// materialを両面に設定する
 	tfloor.material.side = THREE.DoubleSide;
-	tfloor.receiveShadow = true;
+	// 影の設定を行う
+	tfloor.receiveShadow = true;		// 影が投影される
+
+	// 作成したmeshをリターンする
 	return tfloor;
 }
